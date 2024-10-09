52f36b19a705bfcef25dc9f535a77c88d2283a18
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2024-10-09 17:36:58 +0200

    pre mice

--- game.js
+++ game.js
@@ -1,17 +1,13 @@
 console.log('no need to hide')
 var canvas = document.getElementById('canvas')
 const ctx = canvas.getContext("2d");
-var mice = [{x: 0, y: 0}];
-var gamepads = navigator.getGamepads();
+var mice = [{x: 0, y: 0, isAnyButtonPressed: false}];
+var virtualGamepads = []
 var stop = false;
 var frameCount = 0;
 var startTime, then, now, dt, fps=0, fpsTime
 var dtFix = 10, dtToProcess = 0
 var figures = []
-
-const distance = (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1); 
-const angle = (x1, y1, x2, y2) => Math.atan2(y2 - y1, x2 - x1); 
-const move = (x1, y1, angle, speed) => ({x: x1 + Math.cos(angle)*speed, y: y1 + Math.sin(angle)*speed}); 
 
 document.addEventListener("DOMContentLoaded", function(event){
     resizeCanvasToDisplaySize(canvas)
@@ -28,9 +24,12 @@ document.addEventListener("DOMContentLoaded", function(event){
             y: Math.random()*canvas.height,
             xTarget: Math.random()*canvas.width,
             yTarget: Math.random()*canvas.height,
-            speed: 0.8,
+            maxSpeed: 0.08,
+            speed: 0,
             isAlive: true, 
-            isAI: i > 2
+            isAI: i > 4,
+            index: i,
+            angle: 0
         })
     }
 
@@ -40,26 +39,14 @@ document.addEventListener("DOMContentLoaded", function(event){
 canvas.addEventListener('pointermove', event => {
     mice[0].x = event.clientX - canvas.offsetLeft;
     mice[0].y = event.clientY -  canvas.offsetTop;
+   // mice[0].isAnyButtonPressed = event.buttons.some(b => b.pressed)
+
+
 }, false);
 
 window.addEventListener("resize", function(event){
     resizeCanvasToDisplaySize(canvas)
 });
-
-function resizeCanvasToDisplaySize(canvas) {
-    // look up the size the canvas is being displayed
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
- 
-    // If it's resolution does not match change it
-    if (canvas.width !== width || canvas.height !== height) {
-      canvas.width = width;
-      canvas.height = height;
-      return true;
-    }
- 
-    return false;
- }
 
 
 function gameLoop() {
@@ -69,24 +56,60 @@ function gameLoop() {
         fpsTime = now
         fps = Math.floor(1000/dt)
     }
-    gamepads = navigator.getGamepads().filter(x => x && x.connected);
+    virtualGamepads = navigator.getGamepads().filter(x => x && x.connected).map(g => {
+        g.isAnyButtonPressed = g.buttons.some(b => b.pressed)
+        let x = g.axes[0];
+        let y = g.axes[1];
+        [x, y] = setDeadzone(x, y,0.0001);
+        [x, y] = clampStick(x, y);
+        g.xAxis = x
+        g.yAxis = y
+        g.isMoving = Math.abs(x) > 0 && Math.abs(y) > 0.0001
+        return g
+    });
+
+   /* mice.forEach(m => {
+        g = {}
+        let x = m.x - canvas.x / 2;
+        let y = m.y - canvas.y / 2;
+        [x, y] = setDeadzone(x, y,0.0001);
+        [x, y] = clampStick(x, y);
+        g.xAxis = x
+        g.yAxis = y
+        g.isMoving = Math.abs(x) > 0 && Math.abs(y) > 0.0001
+        virtualGamepads.unshift(g)
+    })*/
+   
 
     dtToProcess += dt
     while(dtToProcess > dtFix) {
-        handleInput(gamepads, mice, dtFix, figures)
+        handleInput(virtualGamepads, figures)
         handleAi(figures)
+        updateGame(figures, dtFix)
         dtToProcess-=dtFix
     }
     
-    draw(gamepads, mice, figures, dt);
+    draw(virtualGamepads, mice, figures, dt);
 
 
     then = now
     window.requestAnimationFrame(gameLoop);
 }
 
-function handleInput(gamepads, mice, dtFix, figures) {
+function updateGame(figures, dt) {
+    figures.forEach(f => {
+        let xyNew = move(f.x, f.y, f.angle,f.speed, dt)
+        f.x = xyNew.x
+        f.y = xyNew.y
+    })
+}
 
+function handleInput(virtualGamepads, figures) {
+    virtualGamepads.forEach((g,i) => {
+        var f = figures[i]
+        f.angle = angle(0,0,g.xAxis,g.yAxis)
+        f.speed = g.isMoving ? f.maxSpeed : 0.0
+    })
 }
 
 function handleAi(figures) {
@@ -95,10 +118,8 @@ function handleAi(figures) {
             f.xTarget = Math.random()*canvas.width
             f.yTarget = Math.random()*canvas.height
         }
-
-        let xyNew = move(f.x, f.y, angle(f.x,f.y,f.xTarget,f.yTarget),f.speed)
-        f.x = xyNew.x
-        f.y = xyNew.y
+        f.angle = angle(f.x,f.y,f.xTarget,f.yTarget)
+        f.speed = f.maxSpeed
     })
 }
 
@@ -108,10 +129,10 @@ function draw(gamepads, mice, figures, dt) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath();
     ctx.arc(mice[0].x, mice[0].y, 40, 0, 2 * Math.PI);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "yellow";
     ctx.fill();
     ctx.lineWidth = 4;
-    ctx.strokeStyle = "blue";
+    ctx.strokeStyle = "red";
     ctx.stroke();
 
     figures.forEach(f => {
@@ -119,9 +140,21 @@ function draw(gamepads, mice, figures, dt) {
         ctx.beginPath()
         ctx.lineWidth = 1;
         ctx.fillStyle = "green";
+        if (!f.isAI) {
+            ctx.fillStyle = "red";
+        }
         ctx.arc(f.x, f.y, 5, 0, 2 * Math.PI);
         ctx.fill();
+
+        if (!f.isAI) {
+            ctx.fillStyle = "red";
+            ctx.font = "16px serif";
+            ctx.fillStyle = "white";
+            ctx.fillText(f.index + '',f.x,f.y)
+        }
     })
+
+  
 
 
     ctx.font = "16px serif";
@@ -129,5 +162,7 @@ function draw(gamepads, mice, figures, dt) {
     ctx.textAlign = "left";
     ctx.textBaseline='top'
     ctx.fillText("FPS: " + fps + " Gamepads: " + gamepads.length + " Mouses: " + mice.length + " Time: " + (new Date().getTime() / 1000), 0, 0);
-
+    gamepads.forEach((g,i) => {
+        ctx.fillText("xAxis: " + g.xAxis + " yAxis: " + g.yAxis + " Button?: " + g.isAnyButtonPressed,0,(1+i)*16) 
+    })
 }
---
+++ helper.js
@@ -0,0 +1,51 @@
+/**
+ * Set a value based on the deadzone
+ */
+function setDeadzone(x, y, deadzone=0.2) {
+    let m = Math.sqrt(x*x + y*y);
+
+    if (m < deadzone)
+        return [0, 0];
+
+    let over = m - deadzone;  // 0 -> 1 - deadzone
+    let nover = over / (1 - deadzone);  // 0 -> 1
+
+    let nx = x / m;
+    let ny = y / m;
+
+    return [nx * nover, ny * nover];
+}
+
+function clampStick(x, y) {
+    // Compute magnitude (length) of vector
+    let m = Math.sqrt(x*x + y*y);
+
+    // If the length greater than 1, normalize it (set it to 1)
+    if (m > 1) {
+        x /= m;
+        y /= m;
+    }
+
+    // Return the (possibly normalized) vector
+    return [x, y];
+}
+
+
+const distance = (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1); 
+const angle = (x1, y1, x2, y2) => Math.atan2(y2 - y1, x2 - x1); 
+const move = (x1, y1, angle, speed) => ({x: x1 + Math.cos(angle)*speed*dt, y: y1 + Math.sin(angle)*speed*dt}); 
+
+function resizeCanvasToDisplaySize(canvas) {
+    // look up the size the canvas is being displayed
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+ 
+    // If it's resolution does not match change it
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true;
+    }
+ 
+    return false;
+ }
