2a3fbb5113c7be6935e4a7831058cdcd5db24550
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2024-10-27 11:18:20 +0100

    new file for webgl test

---
+++ misc/webgl.js
@@ -0,0 +1,47 @@
+var canvas = document.getElementById('canvas')
+const ctx = canvas.getContext("2d");
+var startTime, then, now, dt, fps=0, fpsMinForEffects=30, fpsTime=0, dtFix = 10, dtToProcess = 0, dtProcessed = 0
+
+document.addEventListener("DOMContentLoaded", function(event){
+    resizeCanvasToDisplaySize(canvas)
+
+    window.requestAnimationFrame(gameLoop);
+})
+
+
+
+function gameLoop() {
+    now = Date.now();
+    dt = now - then;
+    if (fpsTime < now - 1000) {
+        fpsTime = now
+        fps = Math.floor(1000/dt)
+    }
+   
+    dtToProcess += dt
+    while(dtToProcess > dtFix) {
+        dtToProcess-=dtFix
+        dtProcessed+=dtFix
+    }
+    
+    draw(dt, dtProcessed);
+    then = now
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+
+function draw(dt, dtProcessed) {
+    ctx.save()
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.save()
+            ctx.font = "16px serif";
+            ctx.fillStyle = "white";
+            ctx.textBaseline='top'
+            ctx.textAlign = "left";
+            ctx.fillText(fps + " FPS ",0,0)
+        ctx.restore()
+
+    ctx.restore()
+}
