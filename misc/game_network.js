c015d3e8252eaefb614ac4cc24f61b31ddcd3f8b
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2024-11-04 10:31:52 +0100

    network

--- misc/game_network.js
+++ misc/game_network.js
@@ -38,26 +38,15 @@ async function initApp() {
     sprite.height = canvas.height/5
     container.addChild(sprite)
     
-    const bitmapFontText = new PIXI.BitmapText({
-        text: 'Knirps und Knall',
-        style: {
-            fontFamily: 'Desyrel',
-            fontSize: 32,
-            align: 'left',
-        }
-    });
+    const bitmapFontText = createBitmapText({text: 'Knirps und Knall', style:{fontSize:32}})
 
     bitmapFontText.anchor.set(0.5,0.5)
     bitmapFontText.x = canvas.width/2
     bitmapFontText.y =  sprite.height/2
     container.addChild(bitmapFontText);
 
-
-
     now = Date.now();
     then = Date.now()
-
-
 
     btnSend
     .addEventListener('click', function (event) {
@@ -100,12 +89,6 @@ async function initApp() {
 
 initApp()
 
-
-
-
-
-
-
 function handleInput(dt) {
     players.filter(p => p.networkIndex === networkIndexLocal).forEach((p,i) => {
         var keyboard = keyboards[i % 2]
@@ -140,9 +123,6 @@ function update(dt) {
         if (f.y > canvas.height) f.y = canvas.height
         if (f.x < 0) f.x = 0
         if (f.y < 0) f.y = 0
-
-        f.container.x = f.x
-        f.container.y = f.y
     })
 }
 
@@ -157,23 +137,6 @@ function render() {
     document.getElementById('txtPeerId').innerText = peer?.id
 }
 
-function textareaLog(d) {
-    var textArea = document.getElementById('textAreaLog')
-    textArea.value += d + '\n';
-    textArea.scrollTop = textArea.scrollHeight;
-    var color = 'white'
-    if (peer && peer.open) {
-        if (isMaster(peer)) {
-            color = 'gold'
-        } else {
-            color = 'silver'
-        }
-    } 
-
-    document.getElementsByTagName('body')[0].style.backgroundColor = color
-    console.log(d)
-}
-
 function joinLocalPlayer() {
     if (networkIndexLocal < 0) {
         sendMessageBufferToAllPeers(getMessageBufferWantNetworkIndexes())
@@ -181,129 +144,61 @@ function joinLocalPlayer() {
     }
     var playerIndex = players.length
     var player = {networkIndex: networkIndexLocal, playerIndex: playerIndex, type: 'keyboard', xAxis: 0, yAxis: 0 }
-    var figure = {networkIndex: networkIndexLocal, playerIndex: playerIndex, x: Math.random()*320, y:Math.random()*320, angle: 0}
+    var figure =  createFigure({networkIndex: networkIndexLocal, playerIndex: playerIndex, x: Math.random()*320, y:Math.random()*320, angle: 0})
+
 
     players.push(player)
     figures.push(figure)
+    app.stage.addChild(figure)
+}
 
 
-    let container = new PIXI.Container()
-    container.x = figure.x
-    container.y = figure.y
-    app.stage.addChild(container)
+function createFigure(props) {
+    var f = new PIXI.Container()
+    Object_assign(f, props)
 
     let sprite = PIXI.Sprite.from('../gfx/character_base_topview_32x32.png');
     sprite.anchor.set(0.5)
     sprite.x = 0
     sprite.y = 0
     sprite.scale = 0.25
-    container.addChild(sprite)
 
-    const bitmapFontText = new PIXI.BitmapText({
-        text: figure.networkIndex + ':' + figure.playerIndex,
+    const bitmapFontText =createBitmapText({
+        text: f.networkIndex + ':' + f.playerIndex,
         style: {
-            fontFamily: 'Desyrel',
             fontSize: 24,
             align: 'center',
         }
     });
-
+    
     bitmapFontText.anchor.set(0.5)
     bitmapFontText.x = 0//app.screen.width/2
     bitmapFontText.y = 0//app.screen.height/2
-    container.addChild(bitmapFontText);
 
-    figure.container = container
+    
+    f.addChild(sprite)
+    f.addChild(bitmapFontText);
+
+    return f
 }
 
-
-function dataReceived(d, peer, conn) {
-    var jsonObject = d
- 
-    if (jsonObject.players) {
-        jsonObject.players.filter(p => p.networkIndex !== networkIndexLocal).forEach(p => {
-            var indexInArray = players.findIndex(pp => pp.networkIndex === p.networkIndex && pp.playerIndex === p.playerIndex)
-            if (indexInArray >= 0) {
-                players[indexInArray] = k
-            } else {
-                players.push(p)
-            }
-        })
-    }
-
-    if (jsonObject.figures) {
-        jsonObject.figures.filter(p => p.networkIndex !== networkIndexLocal).forEach(p => {
-            var indexInArray = figures.findIndex(pp => pp.networkIndex === p.networkIndex && pp.playerIndex === p.playerIndex)
-            if (indexInArray >= 0) {
-                figures[indexInArray] = k
-            } else {
-                figures.push(p)
-            }
-        })
-    }
+function createBean() {
+    var f = new PIXI.Container()
+    f.addChild(createBitmapText('B'))
+    return f
 }
 
-function getPlayersAndFiguresDataToSend() {
-    var jsonObject = {
+function createBitmapText(props) {
+    var t = new PIXI.BitmapText({
+        text: '',
+        style: {
+            fontFamily: 'Desyrel',
+            fontSize: 16,
+            align: 'left',
+        }
+    });
 
-    }
+    Object_assign(t, props)
 
-    if (isMaster(peer)) {
-        jsonObject.figures = figures
-        jsonObject.players = players
-    } else {
-        jsonObject.players = players.filter(p => p.networkIndex === networkIndexLocal)
-        jsonObject.figures = figures.filter(p => p.networkIndex === networkIndexLocal)
-    }
-
-    return jsonObject
-}
-
-function sendData() {
-   
-}
-
-
-function sendMessagePlayersAndFigures(peerId, peer, conn) {
-    const dataToSend = getPlayersAndFiguresDataToSend()
-    const figuresToSend = dataToSend.figures
-    const playersToSend = dataToSend.players
-    const playerPayloadBytes = 4+1+1+1 // 2* floatUnitCircle aka 2 Byte (2*2 Bytes) + 1 * boolean (isActionButtonPressed, 1 Byte) + 1* int8 (playerIndex) + 1*int8 (networkIndex)
-    const figurePayloadBytes = 1+1+4+2 // 1*int8 (networkIndex) + 1* int8 (playerIndex) + 4  float3000 aka int16 (x/y) + 2 floatAngle aka int16 (angle)
-    const payloadLength = 1+(1+figuresToSend.length*figurePayloadBytes) + (1+playersToSend.length*playerPayloadBytes)
-
-    let buffer = new ArrayBuffer(payloadLength)
-    let view = new DataView(buffer)
-    var offset = 0
-
-    // message id
-    view.setUint8(0, PEERMESSAGEID_PLAYERSANDFIGURES)
-    offset+=1
-
-    // number of players
-    view.setUint8(1, playersToSend.length)
-    offset+=1
-
-    playersToSend.forEach((p) => {
-        writeUnitCircleFloatAsInt16(buffer, offset + 0, p.xAxis)
-        writeUnitCircleFloatAsInt16(buffer, offset + 2, p.yAxis)
-        view.setUint8(offset + 4, p.isActionButtonPressed ? 1 : 0)
-        view.setUint8(offset + 5, p.networkIndex)
-        view.setUint8(offset + 6, p.playerIndex)
-        offset += playerPayloadBytes
-    })
-
-    // number of figures
-    view.setUint8(2, figuresToSend.length)
-    offset+=1
-
-    figuresToSend.forEach((f) => {
-        view.setUint8(offset + 0, f.networkIndex)
-        view.setUint8(offset + 1, f.playerIndex)
-        write3000erFloatAsInt16(buffer, offset + 2, f.x)
-        write3000erFloatAsInt16(buffer, offset + 4, f.y)
-        writeAngleAsInt16(buffer, offset + 6, f.angle)
-        offset+=figurePayloadBytes
-    })
-    conn.send(buffer)
+    return t
 }
---
+++ misc/helper_network.js
@@ -0,0 +1,123 @@
+
+function sendMessagePlayersAndFigures(peerId, peer, conn) {
+    const dataToSend = getPlayersAndFiguresDataToSend()
+    const figuresToSend = dataToSend.figures
+    const playersToSend = dataToSend.players
+    const playerPayloadBytes = 4+1+1+1 // 2* floatUnitCircle aka 2 Byte (2*2 Bytes) + 1 * boolean (isActionButtonPressed, 1 Byte) + 1* int8 (playerIndex) + 1*int8 (networkIndex)
+    const figurePayloadBytes = 1+1+4+2 // 1*int8 (networkIndex) + 1* int8 (playerIndex) + 4  float3000 aka int16 (x/y) + 2 floatAngle aka int16 (angle)
+    const payloadLength = 1+(1+figuresToSend.length*figurePayloadBytes) + (1+playersToSend.length*playerPayloadBytes)
+
+    let buffer = new ArrayBuffer(payloadLength)
+    let view = new DataView(buffer)
+    var offset = 0
+
+    // message id
+    view.setUint8(0, PEERMESSAGEID_PLAYERSANDFIGURES)
+    offset+=1
+
+    // number of players
+    view.setUint8(1, playersToSend.length)
+    offset+=1
+
+    playersToSend.forEach((p) => {
+        writeUnitCircleFloatAsInt16(buffer, offset + 0, p.xAxis)
+        writeUnitCircleFloatAsInt16(buffer, offset + 2, p.yAxis)
+        view.setUint8(offset + 4, p.isActionButtonPressed ? 1 : 0)
+        view.setUint8(offset + 5, p.networkIndex)
+        view.setUint8(offset + 6, p.playerIndex)
+        offset += playerPayloadBytes
+    })
+
+    // number of figures
+    view.setUint8(2, figuresToSend.length)
+    offset+=1
+
+    figuresToSend.forEach((f) => {
+        view.setUint8(offset + 0, f.networkIndex)
+        view.setUint8(offset + 1, f.playerIndex)
+        write3000erFloatAsInt16(buffer, offset + 2, f.x)
+        write3000erFloatAsInt16(buffer, offset + 4, f.y)
+        writeAngleAsInt16(buffer, offset + 6, f.angle)
+        offset+=figurePayloadBytes
+    })
+    conn.send(buffer)
+}
+
+
+function dataReceived(d, peer, conn) {
+    var jsonObject = d
+ 
+    if (jsonObject.players) {
+        jsonObject.players.filter(p => p.networkIndex !== networkIndexLocal).forEach(p => {
+            var indexInArray = players.findIndex(pp => pp.networkIndex === p.networkIndex && pp.playerIndex === p.playerIndex)
+            if (indexInArray >= 0) {
+                players[indexInArray] = k
+            } else {
+                players.push(p)
+            }
+        })
+    }
+
+    if (jsonObject.figures) {
+        jsonObject.figures.filter(p => p.networkIndex !== networkIndexLocal).forEach(p => {
+            var indexInArray = figures.findIndex(pp => pp.networkIndex === p.networkIndex && pp.playerIndex === p.playerIndex)
+            if (indexInArray >= 0) {
+                figures[indexInArray] = k
+            } else {
+                figures.push(p)
+            }
+        })
+    }
+}
+
+function getPlayersAndFiguresDataToSend() {
+    var jsonObject = {
+
+    }
+
+    if (isMaster(peer)) {
+        jsonObject.figures = figures
+        jsonObject.players = players
+    } else {
+        jsonObject.players = players.filter(p => p.networkIndex === networkIndexLocal)
+        jsonObject.figures = figures.filter(p => p.networkIndex === networkIndexLocal)
+    }
+
+    return jsonObject
+}
+
+function sendData() {
+   
+}
+
+
+function textareaLog(d) {
+    var textArea = document.getElementById('textAreaLog')
+    textArea.value += d + '\n';
+    textArea.scrollTop = textArea.scrollHeight;
+    var color = 'white'
+    if (peer && peer.open) {
+        if (isMaster(peer)) {
+            color = 'gold'
+        } else {
+            color = 'silver'
+        }
+    } 
+
+    document.getElementsByTagName('body')[0].style.backgroundColor = color
+    console.log(d)
+}
+
+function Object_assign (target, ...sources) {
+    sources.forEach(source => {
+      Object.keys(source).forEach(key => {
+        const s_val = source[key]
+        const t_val = target[key]
+        target[key] = t_val && s_val && typeof t_val === 'object' && typeof s_val === 'object'
+                    ? Object_assign(t_val, s_val)
+                    : s_val
+      })
+    })
+    return target
+  }
+  
