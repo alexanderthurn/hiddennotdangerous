131d7bc9a8a43b97794765c73b5b91372c7c1dcb
Alexander Thurn <alexander.thurn@frontendsolutions.de>
2025-02-22 13:59:43 +0100

    rotate

--- network-light/controller.js
+++ network-light/controller.js
@@ -90,7 +90,29 @@ async function init() {
     app.containerGame.addChild(touchControl)
     app.finishLoading()
    
+
     app.ticker.add((ticker) => {
+
+        const isPortrait = app.screen.width < app.screen.height;
+    
+       // app.containerGame.y = app.screen.height*0.25;
+        //app.containerGame.pivot.set(app.screen.width*0.5, app.screen.height*0.5);
+
+
+        if (isPortrait) {
+            app.containerGame.angle = -270 - ticker.lastTime*0.0;
+            app.containerGame.x = app.screen.width
+            //app.containerGame.scale.set( app.screen.width / app.screen.height, app.screen.width / app.screen.height);
+            app.containerGame.screenWidth = app.screen.height;
+            app.containerGame.screenHeight = app.screen.width;    
+        } else {
+            app.containerGame.angle = 0;
+            app.containerGame.x = 0
+            app.containerGame.scale.set(1, 1);
+            app.containerGame.screenWidth = app.screen.width;
+            app.containerGame.screenHeight = app.screen.height;
+        }
+
         main(app, ticker)
     })
 
@@ -105,58 +127,6 @@ window.addEventListener("load", (event) => {
 function main(app, ticker) {
     touchControl.update(app, ticker)
 }
-   /*
-   axes: [0, 0, 0, 0]
-   buttons: []
-            Gamepads0: A
-            Gamepads1: B
-            Gamepads2: X
-            Gamepads3: Y
-            Gamepads4: LB (Left Bumper)
-            Gamepads5: RB (Right Bumper)
-            Gamepads6: LT (Left Trigger)
-            Gamepads7: RT (Right Trigger)
-            Gamepads8: Back
-            Gamepads9: Start
-            Gamepads10: Left Stick (Click)
-            Gamepads11: Right Stick (Click)
-            Gamepads12: D-Pad Up
-            Gamepads13: D-Pad Down
-            Gamepads14: D-Pad Left
-            Gamepads15: D-Pad Right
-            Gamepads16: Guide (Home Button, optional)
-        */
-
-            /* 
-
-
-Type	Index	Location
-Button	
-0	Bottom button in right cluster
-1	Right button in right cluster
-2	Left button in right cluster
-3	Top button in right cluster
-4	Top left front button
-5	Top right front button
-6	Bottom left front button
-7	Bottom right front button
-8	Left button in center cluster
-9	Right button in center cluster
-10	Left stick pressed button
-11	Right stick pressed button
-12	Top button in left cluster
-13	Bottom button in left cluster
-14	Left button in left cluster
-15	Right button in left cluster
-16	Center button in center cluster
-axes	
-0	Horizontal axis for left stick (negative left/positive right)
-1	Vertical axis for left stick (negative up/positive down)
-2	Horizontal axis for right stick (negative left/positive right)
-3	Vertical axis for right stick (negative up/positive down)
-
-
-            */
 
 class NetworkGamepad {
     constructor() {
@@ -241,7 +211,7 @@ class FWTouchControl extends PIXI.Container{
             axisContainer.yAxis = 0
             switch(i) {
                 case 0: axisContainer.rPos = [0.0, 1.0, 0.2]; break;
-                case 1: axisContainer.rPos = [0.5, 1.0, 0.1]; break;
+                case 1: axisContainer.rPos = [0.0, 1.0, 0.1, 2.0]; break;
             }
             this.axesContainers.push(axisContainer)
             this.addChild(axisContainer)
@@ -266,24 +236,24 @@ class FWTouchControl extends PIXI.Container{
                 case 2: buttonText.text = 'X'; buttonContainer.rPos = [0.7, 0.85, 0.075, -2.0, 0.0]; break;
                 case 3: buttonText.text = 'Y'; buttonContainer.rPos = [0.85, 0.7,0.075, -1.0, 0.0]; break;
                 */
-               
+
             switch(i) {
-                case 0: buttonText.text = 'A'; buttonContainer.rPos = [0.85, 1.0, 0.075, -1.0, 0.0]; break;
+                case 0: buttonText.text = 'A'; buttonContainer.rPos = [1.0, 1.0, 0.075, -1.0, 0.0]; break;
                 case 1: buttonText.text = 'B'; buttonContainer.rPos = [1.0, 0.85, 0.075, 0.0, 0.0]; break;
-                case 2: buttonText.text = 'X'; buttonContainer.rPos = [0.7, 0.85, 0.075, -2.0, 0.0]; break;
-                case 3: buttonText.text = 'Y'; buttonContainer.rPos = [0.85, 0.7,0.075, -1.0, 0.0]; break;
-                case 4: buttonText.text = 'LB'; buttonContainer.rPos = [0.0, 0.0,0.05]; break;
+                case 2: buttonText.text = 'X'; buttonContainer.rPos = [1.0, 0.85, 0.075, -2.0, 0.0]; break;
+                case 3: buttonText.text = 'Y'; buttonContainer.rPos = [1.0, 0.7,0.075, -1.0, 0.0]; break;
+                case 4: buttonText.text = 'LB'; buttonContainer.rPos = [0.8, 0.0,0.05]; break;
                 case 5: buttonText.text = 'RB'; buttonContainer.rPos = [1.0, 0.0,0.05]; break;
-                case 6: buttonText.text = 'LT'; buttonContainer.rPos = [0.05, 0.1,0.05]; break;
+                case 6: buttonText.text = 'LT'; buttonContainer.rPos = [0.85, 0.1,0.05]; break;
                 case 7: buttonText.text = 'RT'; buttonContainer.rPos = [0.95, 0.1,0.05]; break;
                 case 8: buttonText.text = 'SELECT'; buttonContainer.rPos = [0.4, 0.5,0.075]; break;
                 case 9: buttonText.text = 'START'; buttonContainer.rPos = [0.6, 0.5,0.075]; break;
-                case 10: buttonText.text = 'AXIS_0_CLICK'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
-                case 11: buttonText.text = 'AXIS_1_CLICK'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
-                case 12: buttonText.text = 'DPAD_UP'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
-                case 13: buttonText.text = 'DPAD_DOWN'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
-                case 14: buttonText.text = 'DPAD_LEFT'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
-                case 15: buttonText.text = 'DPAD_RIGHT'; buttonContainer.rPos = [0.85, 0.7,0.075]; break;
+                case 10: buttonText.text = 'A1'; buttonContainer.rPos = [0.5, 1.0,0.05, -0.5]; break;
+                case 11: buttonText.text = 'A2'; buttonContainer.rPos = [0.5, 1.0,0.05, 0.5]; break;
+                case 12: buttonText.text = 'v'; buttonContainer.rPos = [0.0, 0.2,0.075,1.0, 1.0]; break;
+                case 13: buttonText.text = '^'; buttonContainer.rPos = [0.0, 0.2,0.075, 1.0, -1.0]; break;
+                case 14: buttonText.text = '<'; buttonContainer.rPos = [0.0, 0.2,0.075]; break;
+                case 15: buttonText.text = '>'; buttonContainer.rPos = [0.0, 0.2,0.075, 2.0, 0.0]; break;
                 case 16: buttonText.text = 'HOME'; buttonContainer.rPos = [0.5, 0.3,0.075]; break;
             }
         }
@@ -336,22 +306,23 @@ class FWTouchControl extends PIXI.Container{
 
 
     update(app, ticker) {
-        let minHeightWidth = Math.min(app.screen.width, app.screen.height)
-        let maxHeightWidth = Math.max(app.screen.width, app.screen.height)
+        let minHeightWidth = Math.min(app.containerGame.screenWidth, app.containerGame.screenHeight)
+        let maxHeightWidth = Math.max(app.containerGame.screenWidth, app.containerGame.screenHeight)
+        //minHeightWidth = maxHeightWidth
         const distanceToBorder = 0.05*minHeightWidth
 
         this.axesContainers.forEach((container, index) => {
             container.radius = container.rPos[2]*minHeightWidth
             container.scale = container.radius/container.startRadius
-            container.x = (distanceToBorder + container.radius) + container.rPos[0]*(app.screen.width - distanceToBorder*2 -container.radius*2)
-            container.y = (distanceToBorder + container.radius) + container.rPos[1]*(app.screen.height - distanceToBorder*2 -container.radius*2)
+            container.x = (distanceToBorder + container.radius) + container.rPos[0]*(app.containerGame.screenWidth - distanceToBorder*2 -container.radius*2) + (container.rPos.length > 3 ? container.rPos[3]*container.radius*2 : 0)
+            container.y = (distanceToBorder + container.radius) + container.rPos[1]*(app.containerGame.screenHeight - distanceToBorder*2 -container.radius*2) + (container.rPos.length > 4 ? container.rPos[4]*container.radius*2 : 0)
        })
 
         this.buttonContainers.forEach((container, index) => {
             container.radius = container.rPos[2]*minHeightWidth
             container.scale = container.radius/container.startRadius
-            container.x = (distanceToBorder + container.radius) + container.rPos[0]*(app.screen.width - distanceToBorder*2 -container.radius*2)
-            container.y = (distanceToBorder + container.radius) + container.rPos[1]*(app.screen.height - distanceToBorder*2 -container.radius*2)
+            container.x = (distanceToBorder + container.radius) + container.rPos[0]*(app.containerGame.screenWidth - distanceToBorder*2 -container.radius*2) + (container.rPos.length > 3 ? container.rPos[3]*container.radius*2 : 0)
+            container.y = (distanceToBorder + container.radius) + container.rPos[1]*(app.containerGame.screenHeight - distanceToBorder*2 -container.radius*2) + (container.rPos.length > 4 ? container.rPos[4]*container.radius*2 : 0)
         })
 
         //if (!mp.pressed.has(0)) {
@@ -366,3 +337,58 @@ class FWTouchControl extends PIXI.Container{
     }
 
 }
+
+
+
+   /*
+   axes: [0, 0, 0, 0]
+   buttons: []
+            Gamepads0: A
+            Gamepads1: B
+            Gamepads2: X
+            Gamepads3: Y
+            Gamepads4: LB (Left Bumper)
+            Gamepads5: RB (Right Bumper)
+            Gamepads6: LT (Left Trigger)
+            Gamepads7: RT (Right Trigger)
+            Gamepads8: Back
+            Gamepads9: Start
+            Gamepads10: Left Stick (Click)
+            Gamepads11: Right Stick (Click)
+            Gamepads12: D-Pad Up
+            Gamepads13: D-Pad Down
+            Gamepads14: D-Pad Left
+            Gamepads15: D-Pad Right
+            Gamepads16: Guide (Home Button, optional)
+        */
+
+            /* 
+
+
+Type	Index	Location
+Button	
+0	Bottom button in right cluster
+1	Right button in right cluster
+2	Left button in right cluster
+3	Top button in right cluster
+4	Top left front button
+5	Top right front button
+6	Bottom left front button
+7	Bottom right front button
+8	Left button in center cluster
+9	Right button in center cluster
+10	Left stick pressed button
+11	Right stick pressed button
+12	Top button in left cluster
+13	Bottom button in left cluster
+14	Left button in left cluster
+15	Right button in left cluster
+16	Center button in center cluster
+axes	
+0	Horizontal axis for left stick (negative left/positive right)
+1	Vertical axis for left stick (negative up/positive down)
+2	Horizontal axis for right stick (negative left/positive right)
+3	Vertical axis for right stick (negative up/positive down)
+
+
+            */
